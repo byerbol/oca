ec9c0e819b387119566ad4acbaa7dd0e8602a590
byerbol <yerbol.timuruly@gmail.com>
2020-12-04 12:29:39 +0600

    p-80

--- chapter-1/Main.java
+++ chapter-1/Main.java
@@ -1,6 +1,35 @@
 public class Main {
+
+	// instance or class variables
+	// fields
+	private static char c = 'A';
+
 	public static void main(String[] args) {
-		int a = 1_000_000;
-		System.out.println(a + "$");
+		
+		//case sensetive ? try != Try
+
+		// local variables
+		long Try = 4;
+		System.out.println(Try);
+
+		// const & goto
+		// int const = 5;
+		// System.out.println(const);
+
+		// String goto = "goto";
+
+		// compilable interesting (-_-)
+		String меніңАтым = "Yerbol";
+		System.out.println(меніңАтым);
+
+		System.out.println(c);
+
+		print(c);
+	}
+
+
+	// two local variables
+	private static void print(char c) {
+		System.out.println(c);
 	}
 }
