34041c1b59ce1939bdb670b50c49f81a7033de0e
byerbol <yerbol.timuruly@gmail.com>
2020-12-11 17:02:44 +0600

    r-q2

---
+++ chapter-2/RQ.java
@@ -0,0 +1,102 @@
+public class RQ {
+	public static void main(String[] args) {
+
+		// boolean x = true, z = false;
+		// int y = 20;
+		// x = (y != 10) & (z=true); // z returns true
+
+		// System.out.println(x); // true
+
+		// short a = 3, b = 5;
+		// short c = a + b;
+		// System.out.println(c); // error: incompatible types: possible lossy conversion from int to byte
+
+
+		// boolean b = false;
+
+		// // serioznaa
+		// if(b = true) {
+		// 	System.out.println("Success!");
+		// }else {
+		// 	System.out.println("Failure!");
+		// }
+
+
+		// out of scope
+		// do {
+		// 	int y = 1;
+		// 	y++;
+
+		// }while(y <= 10);
+
+		// short s = 4;
+		// int n = s * 5;
+		// System.out.println(s);
+
+		// int a = 4, b = 5;
+		// double c = a + b;
+
+		// // literal is automate to int
+		// // jogarydan tomen bolsa cast jasau kk.
+		// // kerisinwe kerek emes.
+		// System.out.println(c);
+
+		// int x = 4;
+		// long y = x * 4 - x++;
+		// if(y < 10) System.out.println("1");
+		// else System.out.println("2");
+		// else System.out.println("3"); // last line
+		// int x = 5;
+
+		// System.out.println(x > 2 ? x < 4 ? 10 : 8 : 7); // srsly maan ?
+
+
+
+		// boolean x = true, z = true;
+		// int y = 20;
+		// x = (y != 10) ^ (z=false); // vaweee rabotaet ee
+		// System.out.println(x+", "+y+", "+z);
+
+		// infinite waw?
+		// int cnt = 0;
+		// for(int i=0; i<10 ; ) {
+		// 	i = i++; // i doesn't change
+		// 	System.out.println("Hello World!");
+		// 	cnt++;
+		// }
+
+		// System.out.println(cnt);
+
+		// byte a = 40, b = 50;
+		// byte sum = (byte) a + b; // why lossy conversion from int to byte ?
+		// System.out.println(sum);
+
+
+		// int x = 5 * 4 % 3;
+		// System.out.println(x);
+
+		// int x = 0;
+		// String s = null;
+		// if(x == s) System.out.println("S");
+		// else System.out.println("F");
+
+		// int x = 1;
+
+		// while x < 10 // do not compile
+		// 	x++;
+
+		// it's as:
+		// x > 2 ? (x < 4 ? 10 : 8) : 7
+
+		// int x = 0;
+		// while(x++ < 10) {}
+
+		// // 11
+		// System.out.println(x);
+
+		// long x = 10;
+		// int y = 2 * x; // do not compile
+
+		// System.out.println(y);
+	}
+}
