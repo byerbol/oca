5a8373748161a1726b2de1901e7847e4d97c5a5c
byerbol <yerbol.timuruly@gmail.com>
2020-12-16 10:33:01 +0600

    p-200

---
+++ chapter-3/Parser.java
@@ -0,0 +1,11 @@
+class Parser {
+	public static void main(String[] args) {
+		DateTimeFormatter f = DateTimeFormatter.ofPattern("MM dd yyyy");
+		LocalDate date = LocalDate.parse("01 02 2015", f);
+		LocalTime time = LocalTime.parse("11:22");
+		System.out.println(date);
+		// 2015-01-02
+		System.out.println(time);
+		// 11:22
+	}
+}
