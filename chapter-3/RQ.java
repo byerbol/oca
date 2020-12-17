de0b135fc8422f1bd18adb5e8c7d1fe6bdbccc06
byerbol <yerbol.timuruly@gmail.com>
2020-12-17 12:35:36 +0600

    rq-3

---
+++ chapter-3/RQ.java
@@ -0,0 +1,33 @@
+class RQ {
+	public static void main(String[] args) {
+
+		java.util.Date[] dates[] = new java.util.Date[2][];
+
+		// String letters = "abcdef";
+		// System.out.println(letters.length());
+		// System.out.println(letters.charAt(3));
+		// System.out.println(letters.charAt(6));
+
+		// StringBuilder sb = new StringBuilder();
+		// sb.append("aaa").insert(1, "bb").insert(4, "ccc");
+		// System.out.println(sb);
+
+		// String s1 = "java";
+		// StringBuilder s2 = new StringBuilder("java");
+		// if (s1 == s2)
+		// System.out.print("1");
+		// if (s1.equals(s2))
+		// System.out.print("2");
+
+		// int num = 4;
+		// String s = num + 1;
+		// System.out.println(num);
+		// String s = "Hello";
+		//  String t = new String(s);
+		//  if ("Hello".equals(s)) System.out.println("one");
+		//  if (t == s) System.out.println("two");
+		//  if (t.equals(s)) System.out.println("three");
+		//  if ("Hello" == s) System.out.println("four");
+		//  if ("Hello" == t) System.out.println("five");
+	}
+}
