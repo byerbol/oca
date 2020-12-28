cb4aae49dedbffc1c644ffbb2ec5c65d18d83e8e
Yerbol Baigarayev <yerbol.timuruly@gmail.com>
2020-12-28 23:31:05 +0600

    redeclaring private methods

--- chapter-5/Camel.java
+++ chapter-5/Camel.java
@@ -1,23 +1,22 @@
 public class Camel {
-	protected String getNumberOfHumps() {
+	private String getNumberOfHumps() {
 		return "Undefined";
 	}
-}
-public class BactrianCamel extends Camel {
-	private int getNumberOfHumps() { // DOES NOT COMPILE
-		return 2;
+
+	public class BactrianCamel extends Camel {
+		private int getNumberOfHumps() {
+			return 2;
+		}
 	}
-}
 
-/*
+	/*
+		This code compiles without issue. Notice that the return type differs in the child method
+		from String to int . In this example, the method getNumberOfHumps() in the parent class
+		is hidden, so the method in the child class is a new method and not an override of the
+		method in the parent class. As you saw in the previous section, if the method in the parent
+		class were public or protected , the method in the child class would not compile because it
+		would violate two rules of overriding methods. The parent method in this example is
+		private , so there are no such issues.
 
-	In this example, the method in the child class doesn’t compile for two reasons. First, it
-	violates the second rule of overriding methods: the child method must be at least as acces-
-	sible as the parent. In the example, the parent method uses the protected modifi er, but the
-	child method uses the private modifi er, making it less accessible in the child method than
-	in the parent method. It also violates the fourth rule of overriding methods: the return
-	type of the parent method and child method must be covariant. In this example, the
-	return type of the parent method is String , whereas the return type of the child method
-	is int , neither of which is covariant with each other.
-
-*/
+	*/
+}	
