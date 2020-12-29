39d7602bf532b6dafb7697038bd9af1907f45cc0
Yerbol Baigarayev <yerbol.timuruly@gmail.com>
2020-12-29 22:37:03 +0600

    hidding static methods

---
+++ chapter-5/Bear.java
@@ -0,0 +1,14 @@
+public class Bear {
+	public static void eat() {
+		System.out.println("Bear is eating");
+	}
+}
+
+public class Panda extends Bear {
+	public static void eat() {
+		System.out.println("Panda bear is chewing");
+	}
+	public static void main(String[] args) {
+		Panda.eat();
+	}
+}
