d08f6b98fb869ec7413fea2e021b1349402de7d6
Yerbol Baigarayev <yerbol.timuruly@gmail.com>
2020-12-31 20:12:29 +0600

    overriding vs hiding methods

---
+++ chapter-5/Marsupial.java
@@ -0,0 +1,28 @@
+public class Marsupial {
+		public static boolean isBiped() {
+			return false;
+		}
+		public void getMarsupialDescription() {
+			System.out.println("Marsupial walks on two legs: "+isBiped());
+		}
+	}
+	public class Kangaroo extends Marsupial {
+		public static boolean isBiped() {
+			return true;
+		}
+		public void getKangarooDescription() {
+			System.out.println("Kangaroo hops on two legs: "+isBiped());
+		}
+		public static void main(String[] args) {
+			Kangaroo joey = new Kangaroo();
+			joey.getMarsupialDescription();
+			joey.getKangarooDescription();
+		}
+}
+
+
+/*
+	In this example, the code compiles and runs without issue, outputting the following:
+	Marsupial walks on two legs: false
+	Kangaroo hops on two legs: true
+*/
