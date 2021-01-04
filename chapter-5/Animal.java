b4357401e7956d2e0fa0ce9c05ecaa5289bfafab
Yerbol Baigarayev <yerbol.timuruly@gmail.com>
2021-01-04 19:28:46 +0600

    abs class

--- chapter-5/Animal.java
+++ chapter-5/Animal.java
@@ -1,27 +1,13 @@
-public class Animal {
-	private int age;
-	public Animal(int age) {
-		super();
-		this.age = age;
-		}
+public abstract class Animal {
+	protected int age;
+	public void eat() {
+		System.out.println("Animal is eating");
 	}
-public class Zebra extends Animal {
-	public Zebra(int age) {
-		super(age);
-	}
-
-	public Zebra() {
-		this(4);
-	}
+	public abstract String getName();
 }
 
-/*
-
-	In the fi rst class, Animal , the fi rst statement of the constructor is a call to the parent
-	constructor defi ned in java.lang.Object , which takes no arguments. In the second class,
-	Zebra , the fi rst statement of the fi rst constructor is a call to Animal ’s constructor, which
-	takes a single argument. The class Zebra also includes a second no-argument construc-
-	tor that doesn’t call super() but instead calls the other constructor within the Zebra class
-	using this(4) .
-
-*/
+public class Swan extends Animal {
+	public String getName() {
+		return "Swan";
+	}
+}
